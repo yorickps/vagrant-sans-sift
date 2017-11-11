503345ad4a3f56b505cb47bdb39f02307dc0c695
Yorick Poels <yorick.poels@gmail.com>
2017-11-11 23:45:59 +0100

    Rewrite bootstrap script - install lxde

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -5,12 +5,20 @@ wget -q https://github.com/sans-dfir/sift-cli/releases/download/v1.5.1/sift-cli-
 
 #gpg --keyserver pgp.mit.edu --recv-keys 22598A94 &&\
 #sha256sum -c sift-cli-linux.sha256.asc
+sudo -s
+loadkeys be
+apt-get install -y salt-minion lxde xinit &&\
 
-sudo apt-get install -y salt-minion
-sudo systemctl disable salt-minion &&\
-sudo systemctl stop salt-minion
+echo "greeter-show-manual-login=true" > /usr/share/lightdm/lightdm.conf.d/50-show-login.conf
 
-sudo cp -p sift-cli-linux /usr/local/bin/sift &&\
-sudo chmod 755 /usr/local/bin/sift
-sudo sift install --user=ubuntu &&\
-  sudo sift upgrade
+#cp /etc/X11/xinit/xinitrc /home/ubuntu/.xinitrc &&\
+#chown ubuntu: /home/ubuntu/.xinitrc &&\
+#chmod 644 /home/ubuntu/.xinitrc
+
+systemctl disable salt-minion &&\
+systemctl stop salt-minion
+
+cp -p sift-cli-linux /usr/local/bin/sift &&\
+chmod 755 /usr/local/bin/sift
+sift install --user=ubuntu &&\
+sift upgrade
