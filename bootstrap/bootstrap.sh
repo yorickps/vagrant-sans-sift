01a32d79dfbea1aa8a1214dcbed10b085651f07c
Yorick Poels <yorick.poels@gmail.com>
2017-11-12 11:15:53 +0100

    Clean up - add password for graphical login

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -3,17 +3,12 @@
 wget -q https://github.com/sans-dfir/sift-cli/releases/download/v1.5.1/sift-cli-linux -O sift-cli-linux &&\
 wget -q https://github.com/sans-dfir/sift-cli/releases/download/v1.5.1/sift-cli-linux.sha256.asc -O sift-cli-linux.sha256.asc
 
-#gpg --keyserver pgp.mit.edu --recv-keys 22598A94 &&\
-#sha256sum -c sift-cli-linux.sha256.asc
 sudo -s
+useradd -p $(openssl passwd -1 ubuntu) ubuntu
 loadkeys be
-apt-get install -y salt-minion lxde xinit &&\
+apt-get install -y salt-minion lxde xinit 
 
 echo "greeter-show-manual-login=true" > /usr/share/lightdm/lightdm.conf.d/50-show-login.conf
-
-#cp /etc/X11/xinit/xinitrc /home/ubuntu/.xinitrc &&\
-#chown ubuntu: /home/ubuntu/.xinitrc &&\
-#chmod 644 /home/ubuntu/.xinitrc
 
 systemctl disable salt-minion &&\
 systemctl stop salt-minion
