e15b07c5413be8cd989beb9018bddbaeed53822a
Yorick Poels <yorick.poels@gmail.com>
2017-11-13 22:16:19 +0100

    Add testuser and password

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -4,10 +4,13 @@ wget -q https://github.com/sans-dfir/sift-cli/releases/download/v1.5.1/sift-cli-
 wget -q https://github.com/sans-dfir/sift-cli/releases/download/v1.5.1/sift-cli-linux.sha256.asc -O sift-cli-linux.sha256.asc
 
 sudo -s
-useradd -p $(openssl passwd -1 ubuntu) ubuntu
+useradd test -s /bin/bash -m
+chpasswd << 'END'
+test:test
+END
 loadkeys be
 apt-get -y update &&\
-apt-get install -y salt-minion lxde xinit 
+apt-get install -y salt-minion lxde xinit
 
 echo "greeter-show-manual-login=true" > /usr/share/lightdm/lightdm.conf.d/50-show-login.conf
 
@@ -16,5 +19,5 @@ systemctl stop salt-minion
 
 cp -p sift-cli-linux /usr/local/bin/sift &&\
 chmod 755 /usr/local/bin/sift
-sift install --user=ubuntu &&\
+sift install --user=test &&\
 sift upgrade
